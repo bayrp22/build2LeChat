a9eb9ef90b219fa58911a046642c7b41e96add14
bayp22 <bay@searchloscabos.com>
2025-02-13 14:37:56 -0700

    Add footer to the profile page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/header/Header';
 import SearchBar from './components/SearchBar';
 import RecentActivity from './components/RecentActivity';
 import Settings from './components/Settings';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <SearchBar onSearch={handleSearch} />
       <RecentActivity />
       <Settings />
+      <Footer />
     </div>
   );
 }
