d0c063e44c809d9a7fd2a5976276e459f61e03f7
bayp22 <bay@searchloscabos.com>
2025-02-13 16:09:53 -0700

    Add link to register page from login page

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Login from '../components/Login';
+import { Link } from 'react-router-dom';
+import './LoginPage.css';
 
 const LoginPage = () => {
   return (
     <div className="login-page">
       <h1>Login</h1>
       <Login />
+      <div className="register-link">
+        <p>Don't have an account?</p>
+        <Link to="/register">Create an account</Link>
+      </div>
     </div>
   );
 };
